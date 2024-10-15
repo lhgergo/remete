90767554802c17a022563608f016258af82a7012
lhgergo <lefthandgergo@gmail.com>
2024-10-15 17:09:06 +0200

    testing async server

--- test/client_test.R
+++ test/client_test.R
@@ -1,4 +1,5 @@
 library(magrittr)
+library(purrr)
 # # In this file I aim to test the client side of the package
 
 # # testing the "file" interface ----------
@@ -21,6 +22,37 @@ GetBack(taskobj, interface = ifobj_file, simplified_output = T)
 # save(ifobj_gdrive, file = "test/ifobj_gdrive.RData")
 
 load("test/ifobj_gdrive.RData")
+peps_unq <- sapply(1:100000, \(x) sample(rownames(protr::AABLOSUM62), 9) %>% paste0(collapse = ""))
+taskobj <- emhace.tools::RunNetMHCpan_ng(alleles = "HLA-A02:01", peptides = peps_unq, version_number = "4.1",
+                                         software_path = "/home/lhgergo/Programok/netMHCpan-4.1/netMHCpan") |>
+  PackIn(objects = "peps_unq", libraries = c("purrr", "emhace.tools", "magrittr")) %>%
+  SendOut(ifobj_gdrive)
 
-taskobj <- PackIn(sample(1:100, 50)) %>% SendOut(ifobj_gdrive)
+peps_unq <- sapply(1:100000, \(x) sample(rownames(protr::AABLOSUM62), 9) %>% paste0(collapse = ""))
+taskobj2 <- emhace.tools::RunNetMHCpan_ng(alleles = "HLA-A02:01", peptides = peps_unq, version_number = "4.1",
+                                         software_path = "/home/lhgergo/Programok/netMHCpan-4.1/netMHCpan") |>
+  PackIn(objects = "peps_unq", libraries = c("purrr", "emhace.tools", "magrittr")) %>%
+  SendOut(ifobj_gdrive)
+
 GetBack(taskobj, interface = ifobj_gdrive, simplified_output = T)
+GetBack(taskobj2, interface = ifobj_gdrive, simplified_output = T)
+
+# experimenting with async, managing processes from remote -----------
+GetBack(taskobj, interface = ifobj_gdrive, simplified_output = F)
+
+process_query <- PackIn(system("ps aux | grep /usr/bin/R", intern = T)) |> SendOut(ifobj_gdrive)
+process_query_R <- PackIn(system("ps aux | grep /usr/bin/R", intern = T)) |> SendOut(ifobj_gdrive)
+process_query_nnalign <- PackIn(system("ps aux | grep nnalign", intern = T)) |> SendOut(ifobj_gdrive)
+process_kill_nnalign <- PackIn(system("kill 12231", intern = T)) |> SendOut(ifobj_gdrive)
+
+GetBack(process_query_R, ifobj_gdrive)
+GetBack(process_query_nnalign, ifobj_gdrive)
+GetBack(process_kill_nnalign, ifobj_gdrive)
+
+load("test/ifobj_gdrive.RData")
+data("iris")
+taskobj <- filter(iris, Sepal.Length > 4 & Sepal.Length < 4.5) |>
+  PackIn(objects = "iris", libraries = c("dplyr")) |>
+  SendOut(ifobj_gdrive)
+
+GetBack(taskobj, interface = ifobj_gdrive)
--- test/server_test.R
+++ test/server_test.R
@@ -1,3 +1,7 @@
 # In this file I aim to test the server side of the package
 load("test/ifobj_gdrive.RData")
+source("R/functions.R")
 RunServer(interface = ifobj_gdrive)
+
+
+RunServerAsync(interface = ifobj_gdrive)
